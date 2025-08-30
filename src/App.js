f6deab3d3f4eab6a4df249bbf7abf115414b53c6
hawri <hawri@DESKTOP-BMM1386>
2025-08-30 13:37:08 +0300

    STATE

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          HomeWork
+          STATE
         </h1>
         <a
           className="App-link"
